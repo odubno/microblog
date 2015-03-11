e886c6300a742fd39d205fbec12b645a99b4ae81
Oleh Dubno <olehdubno@gmail.com>
2015-03-11 18:43:34 -0400

    working on establishing a data base

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import orm
+from sqlalchemy import event
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
