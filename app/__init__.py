41542beaf03bedfe01b7678e8814b7a0ee54e746
Oleh Dubno <olehdubno@gmail.com>
2015-03-12 13:26:02 -0400

    updated the virtualenv with new extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import orm
-from sqlalchemy import event
 
 app = Flask(__name__)
 app.config.from_object('config')
