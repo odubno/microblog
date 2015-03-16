c0db2c16d8020e616d5bd815a84133453a9c676a
Oleh Dubno <olehdubno@gmail.com>
2015-03-16 14:38:13 -0400

    fixed some bugs in the files

--- db_repository/versions/002_migration.py
+++ db_repository/versions/002_migration.py
@@ -5,10 +5,11 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-user = Table('user', post_meta,
+post = Table('post', post_meta,
     Column('id', Integer, primary_key=True, nullable=False),
-    Column('nickname', String(length=64)),
-    Column('email', String(length=120)),
+    Column('body', String(length=140)),
+    Column('timestamp', DateTime),
+    Column('user_id', Integer),
 )
 
 
@@ -17,11 +18,11 @@ def upgrade(migrate_engine):
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['user'].create()
+    post_meta.tables['post'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['user'].drop()
+    post_meta.tables['post'].drop()
